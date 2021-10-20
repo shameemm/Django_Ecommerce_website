243d8c86c685a8a68edf68e7a4054152d2c70073
Fahad PN <2020cl332@sctce.ac.in>
2021-10-20 18:45:23 +0530

    minor changes

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -102,7 +102,7 @@ JAZZMIN_SETTINGS = {
 
     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
     "usermenu_links": [
-        {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
+        {"name": "Support", "url": "https://github.com/FAHADPN/Django_Ecommerce_website", "new_window": True},
         {"model": "auth.user"}
     ],
 
