0ce265fdcadda5fc5e7ee3f4b07e077238a9f678
Fahad PN <2020cl332@sctce.ac.in>
2021-10-20 14:25:40 +0530

    admin , product
    admin page, product display, product category functionalities added

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,3 +26,4 @@ urlpatterns = [
 ]
 
 urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
--- shop/website/models.py
+++ shop/website/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.db.models.fields import URLField
 from django.db.models.fields.files import ImageField
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+    def __str__(self):
+        return self.category
+
 class items(models.Model):
     name = models.CharField(max_length=100)
     img = models.ImageField(upload_to = 'pics')
     desc = models.TextField()
     price = models.DecimalField(decimal_places=2,max_digits=6)
     offer = models.BooleanField(default=False)
+    qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.CharField(max_length=20)
+    item = models.CharField(max_length=200)
