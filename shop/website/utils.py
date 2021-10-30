8b7309ac6d86e2bb04a65b2a84d2434471ea1ffa
Fahad PN <2020cl332@sctce.ac.in>
2021-10-31 03:07:07 +0530

    Update utils.py

--- shop/website/utils.py
+++ shop/website/utils.py
@@ -43,7 +43,7 @@ def cookieCart(request):
 
 def cartData(request):
 	if request.user.is_authenticated:
-		customer = Customer.objects.get(user = request.user)
+		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
 		cartItems = order.get_cart_items
